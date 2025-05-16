358b2a3f4789bc0779e1d2bb32dfcf69c699550c
Dimas R <174266241+dms-r@users.noreply.github.com>
2025-05-16 01:50:55 +0000

    update homepage

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,10 @@
+var store = [,{
+    "title": "404: Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist :(  ","url": "http://localhost:4000/404.html"
+  },{
+    "title": "About",
+    "excerpt":"This is me  ","url": "http://localhost:4000/about/"
+  },{
+    "title": "Home",
+    "excerpt":"                   Hello, There!      I'm a passionate at technology. Explore my work, read my thoughts, and get in touch.             View My Work       Read My Post          ","url": "http://localhost:4000/index.html"
+  }]
